cc49688c982b944c917fbf028748aa18ace34eee
Dmitry Kravchuk <kravchuckdm@gmail.com>
2015-07-31 17:11:59 +0300

    no message

--- includes/ya-cron-functions.php
+++ includes/ya-cron-functions.php
@@ -56,11 +56,11 @@ function sf_synchronize_products()
                 GROUP BY {$wpdb->posts}.ID
                 ORDER BY {$wpdb->posts}.ID
                 DESC LIMIT 10";
-
+    
     $vessels = $wpdb->get_results( $query );
 
     echo "<pre>";
-    print_r( 'vessels => ' . $vessels ); exit;
+    print_r( $vessels ); exit;
     echo "</pre>";
 
     foreach ( $vessels as $item )

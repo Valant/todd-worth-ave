3db2bd9d2ddb22e652c9b8192f38f492f7b27910
Dmitry Kravchuk <kravchuckdm@gmail.com>
2015-08-03 19:48:35 +0300

    was deleted previous code and test current;

--- includes/ya-cron-functions.php
+++ includes/ya-cron-functions.php
@@ -27,37 +27,6 @@ function sf_synchronize_products()
     $salesForceApi = new SalesForceApi();
     global $wpdb;
 
-//    $SFProductId_key = '';
-//    if ( $salesForceApi->mode == 'dev' ) {
-//        $SFProductId_key = 'SFProductId_sandbox';
-//    } else if ( $salesForceApi->mode == 'prod' ) {
-//        $SFProductId_key = 'SFProductId';
-//    }
-
-// !!! Add new method
-//$current_update_version = $salesForceApi->update_version;
-
-//    $query = "SELECT {$wpdb->posts}.ID as 'post_id',
-//                     {$wpdb->posts}.post_title,
-//                     m1.meta_value as 'vessel_detail',
-//                     m2.meta_value as '$SFProductId_key',
-//                     m3.meta_value as 'Update_Version' FROM {$wpdb->posts}
-//                LEFT JOIN {$wpdb->postmeta} m1
-//                    ON ( {$wpdb->posts}.ID = m1.post_id AND m1.meta_key = 'vessel_detail' )
-//                LEFT JOIN {$wpdb->postmeta} m2
-//                    ON ( {$wpdb->posts}.ID = m2.post_id AND m2.meta_key = '$SFProductId_key' )
-//                LEFT JOIN {$wpdb->postmeta} m3
-//                    ON ( {$wpdb->posts}.ID = m3.post_id AND m3.meta_key = 'Update_Version' )
-//                WHERE
-//                    {$wpdb->posts}.post_type = 'vessel'
-//                AND {$wpdb->posts}.post_status = 'publish'
-//                AND ( m1.meta_value != '' )
-//                AND ( m2.meta_value IS NULL OR m2.meta_value = '' )
-//                AND ( m3.meta_value IS NULL OR m3.meta_value < '$current_update_version' OR m3.meta_value = '' )
-//                GROUP BY {$wpdb->posts}.ID
-//                ORDER BY {$wpdb->posts}.ID
-//                DESC LIMIT 10";
-
     $query = "SELECT {$wpdb->posts}.ID as 'post_id',
                      {$wpdb->posts}.post_title,
                      m.meta_value as '{$salesForceApi->getSyncVersionKey()}' FROM {$wpdb->posts}
@@ -71,18 +40,19 @@ function sf_synchronize_products()
                 ORDER BY {$wpdb->posts}.ID
                 DESC LIMIT 10";
 
-    echo "<pre>";
-    print_r( $query ); exit;
-    echo "</pre>";
-
     $vessels = $wpdb->get_results( $query );
 
     foreach ( $vessels as $item )
     {
-        $SFProductId = get_post_meta( $item->post_id , $SFProductId_key, true );
-        $product_update_version = $current_update_version;
+        $vessel_detail = get_post_meta( $item->post_id , 'vessel_detail', true );
+        if ( !$vessel_detail ) {
+            update_post_meta( $item->post_id, $salesForceApi->getSyncVersionKey(), $salesForceApi->getSyncVersion() );
+            continue;
+        }
 
-        $vessel_detail = unserialize( $item->vessel_detail );
+        $SFProductId = get_post_meta( $item->post_id , $salesForceApi->getSynchIdKey(), true );
+
+        $vessel_detail = unserialize( $vessel_detail );
         $vessel_detail['ForSale'] = true;
         $vessel_detail['Boatname'] = $item->post_title;
 
@@ -92,6 +62,10 @@ function sf_synchronize_products()
         }
 
         $vessel_detail = (object)$vessel_detail;
+
+        echo "<pre>";
+        print_r( $vessel_detail );
+        echo "</pre>";
 
         if ( $SFProductId ) {
             $responce = $salesForceApi->updateProduct( $SFProductId, $vessel_detail );
@@ -105,20 +79,14 @@ function sf_synchronize_products()
             if( $responce['status'] == 'error' ) {
                 update_post_meta( $item->post_id, 'error_message', $responce['message'] );
             } else {
-                $SFProductId = $responce['id'];
+//                $SFProductId = $responce['id'];
+
+                update_post_meta( $item->post_id, $salesForceApi->getSyncVersionKey(), $salesForceApi->getSyncVersion() );
+                update_post_meta( $item->post_id, $salesForceApi->getSynchIdKey(), $responce['id'] );
             }
         }
 
-
-if( $responce['status'] == 'success' ) {
-    update_post_meta( $item->post_id, 'Update_Version', $product_update_version );
-}
-if ( $salesForceApi->mode == 'dev' ) {
-    update_post_meta( $item->post_id, 'SFProductId_sandbox', $SFProductId );
-} else if ( $salesForceApi->mode == 'prod' ) {
-    update_post_meta( $item->post_id, 'SFProductId', $SFProductId );
-}
-
+        print_r( 'finish' ); exit;
 
     }
 

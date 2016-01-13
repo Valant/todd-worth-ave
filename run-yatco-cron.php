092c232a4d7b16a632f7453a3e3536537c8bbb64
Dmitry Kravchuk <kravchuckdm@gmail.com>
2016-01-13 15:48:58 +0200

    test;

--- run-yatco-cron.php
+++ run-yatco-cron.php
@@ -1,28 +1,15 @@
 <?php
-/**
- * WordPress AJAX Process Execution.
- *
- * @package WordPress
- * @subpackage Administration
- *
- * @link https://codex.wordpress.org/AJAX_in_Plugins
- */
 
-/**
- * Executing AJAX process.
- *
- * @since 2.1.0
- */
-define( 'DOING_AJAX', true );
-if ( ! defined( 'WP_ADMIN' ) ) {
+// define( 'DOING_AJAX', true );
+// if ( ! defined( 'WP_ADMIN' ) ) {
 	// define( 'WP_ADMIN', true );
-}
+// }
 
 /** Load WordPress Bootstrap */
 require_once( dirname( dirname( dirname( dirname( __FILE__ ) ) ) ) . '/wp-load.php' );
 
 /** Allow for cross-domain requests (from the frontend). */
-send_origin_headers();
+// send_origin_headers();
 
 // Require an action parameter
 if ( empty( $_REQUEST['action'] ) )
@@ -34,67 +21,8 @@ require_once( ABSPATH . 'wp-admin/includes/admin.php' );
 /** Load Ajax Handlers for WordPress Core */
 require_once( ABSPATH . 'wp-admin/includes/ajax-actions.php' );
 
-@header( 'Content-Type: text/html; charset=' . get_option( 'blog_charset' ) );
-@header( 'X-Robots-Tag: noindex' );
-
-send_nosniff_header();
-nocache_headers();
-
 /** This action is documented in wp-admin/admin.php */
-do_action( 'admin_init' );
-
-// $core_actions_get = array(
-// 	'fetch-list', 'ajax-tag-search', 'wp-compression-test', 'imgedit-preview', 'oembed-cache',
-// 	'autocomplete-user', 'dashboard-widgets', 'logged-in',
-// );
-
-// $core_actions_post = array(
-// 	'oembed-cache', 'image-editor', 'delete-comment', 'delete-tag', 'delete-link',
-// 	'delete-meta', 'delete-post', 'trash-post', 'untrash-post', 'delete-page', 'dim-comment',
-// 	'add-link-category', 'add-tag', 'get-tagcloud', 'get-comments', 'replyto-comment',
-// 	'edit-comment', 'add-menu-item', 'add-meta', 'add-user', 'closed-postboxes',
-// 	'hidden-columns', 'update-welcome-panel', 'menu-get-metabox', 'wp-link-ajax',
-// 	'menu-locations-save', 'menu-quick-search', 'meta-box-order', 'get-permalink',
-// 	'sample-permalink', 'inline-save', 'inline-save-tax', 'find_posts', 'widgets-order',
-// 	'save-widget', 'set-post-thumbnail', 'date_format', 'time_format', 'wp-fullscreen-save-post',
-// 	'wp-remove-post-lock', 'dismiss-wp-pointer', 'upload-attachment', 'get-attachment',
-// 	'query-attachments', 'save-attachment', 'save-attachment-compat', 'send-link-to-editor',
-// 	'send-attachment-to-editor', 'save-attachment-order', 'heartbeat', 'get-revision-diffs',
-// 	'save-user-color-scheme', 'update-widget', 'query-themes', 'parse-embed', 'set-attachment-thumbnail',
-// 	'parse-media-shortcode', 'destroy-sessions', 'install-plugin', 'update-plugin', 'press-this-save-post',
-// 	'press-this-add-category',
-// );
-
-// Register core Ajax calls.
-// if ( ! empty( $_GET['action'] ) && in_array( $_GET['action'], $core_actions_get ) )
-// 	add_action( 'wp_ajax_' . $_GET['action'], 'wp_ajax_' . str_replace( '-', '_', $_GET['action'] ), 1 );
-
-// if ( ! empty( $_POST['action'] ) && in_array( $_POST['action'], $core_actions_post ) )
-// 	add_action( 'wp_ajax_' . $_POST['action'], 'wp_ajax_' . str_replace( '-', '_', $_POST['action'] ), 1 );
-
-// add_action( 'wp_ajax_nopriv_heartbeat', 'wp_ajax_nopriv_heartbeat', 1 );
-
-// if ( is_user_logged_in() ) {
-	/**
-	 * Fires authenticated AJAX actions for logged-in users.
-	 *
-	 * The dynamic portion of the hook name, `$_REQUEST['action']`,
-	 * refers to the name of the AJAX action callback being fired.
-	 *
-	 * @since 2.1.0
-	 */
-// 	do_action( 'wp_ajax_' . $_REQUEST['action'] );
-// } else {
-	/**
-	 * Fires non-authenticated AJAX actions for logged-out users.
-	 *
-	 * The dynamic portion of the hook name, `$_REQUEST['action']`,
-	 * refers to the name of the AJAX action callback being fired.
-	 *
-	 * @since 2.8.0
-	 */
-// 	do_action( 'wp_ajax_nopriv_' . $_REQUEST['action'] );
-// }
+// do_action( 'admin_init' );
 
 
 	$params = $_REQUEST;
@@ -103,10 +31,6 @@ do_action( 'admin_init' );
 	echo "<pre>";
 	print_r( $params );
 	echo "</pre>";
-
-    // Return error when noonce doesn't match
-    // if ( !wp_verify_nonce($params['noonce'], 'execute_task_'.$params['task']) )
-    //         die( json_encode( array('status' => 'error', 'details' => __('Sorry, wrong noonce.', 'acm')) ) );
 
     $args = array();
     if ( isset( $params['args'] ) && ! empty( $params['args'] ) ) {
@@ -123,7 +47,6 @@ do_action( 'admin_init' );
     } else {
         do_action( $params['task'] );
     }
-
 
 
     die( json_encode( array('status' => 'success') ) );

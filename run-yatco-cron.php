b950a34fb448b6466c164e8fe1b3a6af7275c1e1
Dmitry Kravchuk <kravchuckdm@gmail.com>
2016-01-13 15:51:35 +0200

    test;

--- run-yatco-cron.php
+++ run-yatco-cron.php
@@ -1,15 +1,7 @@
 <?php
 
-// define( 'DOING_AJAX', true );
-// if ( ! defined( 'WP_ADMIN' ) ) {
-	// define( 'WP_ADMIN', true );
-// }
-
 /** Load WordPress Bootstrap */
 require_once( dirname( dirname( dirname( dirname( __FILE__ ) ) ) ) . '/wp-load.php' );
-
-/** Allow for cross-domain requests (from the frontend). */
-// send_origin_headers();
 
 // Require an action parameter
 if ( empty( $_REQUEST['action'] ) )
@@ -19,37 +11,22 @@ if ( empty( $_REQUEST['action'] ) )
 require_once( ABSPATH . 'wp-admin/includes/admin.php' );
 
 /** Load Ajax Handlers for WordPress Core */
-require_once( ABSPATH . 'wp-admin/includes/ajax-actions.php' );
+// require_once( ABSPATH . 'wp-admin/includes/ajax-actions.php' );
 
-/** This action is documented in wp-admin/admin.php */
-// do_action( 'admin_init' );
+$params = $_REQUEST;
 
+$args = array();
+if ( isset( $params['args'] ) && ! empty( $params['args'] ) ) {
+    $args = explode( ",", $params['args'] );
+}
 
-	$params = $_REQUEST;
+if ( $args ) {
+    do_action_ref_array( $params['task'], $args );
+} else {
+    do_action( $params['task'] );
+}
 
-	echo "<params>";
-	echo "<pre>";
-	print_r( $params );
-	echo "</pre>";
-
-    $args = array();
-    if ( isset( $params['args'] ) && ! empty( $params['args'] ) ) {
-        $args = explode( ",", $params['args'] );
-    }
-
-	echo "<args>";
-	echo "<pre>";
-	print_r( $args );
-	echo "</pre>";
-
-    if ( $args ) {
-        do_action_ref_array( $params['task'], $args );
-    } else {
-        do_action( $params['task'] );
-    }
-
-
-    die( json_encode( array('status' => 'success') ) );
+die( json_encode( array('status' => 'success') ) );
 
 // Default status
 die( '0' );
